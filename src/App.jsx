56ca510800bbbc399f2e0205f0d6bfb6557ff2ea
Michael <michaeljimmy.alvarado@gmail.com>
2024-08-31 16:32:32 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+
+function App() {
+  
+
+  return (
+    <div> 
+      
+      <form className='form'>
+      <input placeholder='Avengers, Star wars, Marvel, etc'/>
+      <button type='submit'>Buscar</button>
+      
+      
+        
+      </form>
+
+    </div>
+      
+      
+
+  )
+}
+
+export default App
